04c3bf6b954feda7a282fda309b42c6ec3c25ed1
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-29 23:01:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,12 @@ const codes = [
 let index = 0;
 function init(e){
  const key = e.key;
- 
+ if (key === codes[index]) {
+   index++;
+   if (index === codes.length){
+     alert("Hurray!");
+   }
+ }
  document.body.addEventListener("keydown",(event) =>{
   
 } )
