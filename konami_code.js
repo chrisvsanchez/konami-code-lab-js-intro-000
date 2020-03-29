50c70d64ed25bbe8c485ac36dfbb82ed3d830d90
Chris Sanchez <chrisvsanchez@gmail.com>
2020-03-29 23:05:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,15 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+// const codes = [
+//   "ArrowUp",
+//   "ArrowUp",
+//   "ArrowDown",
+//   "ArrowDown",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "b",
+//   "a"
+// ];
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
